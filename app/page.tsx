b301f738da8444e68a068914a886fa7c3bd6cb67
Mridul Pandey <pandeym891@gmail.com>
2025-07-15 15:04:05 +0530

    Change  in the file system

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+
+import Button from "../registry/default/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <Button size="large" name="Button" variant="default" />
+    </div>
+  );
+}
